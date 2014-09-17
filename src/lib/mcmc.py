a84a1150daac0bb9b9a6756cb824491ba344bc65
Catherine Beauchemin <cbeau@users.sourceforge.net>
2014-09-17 17:18:29 -0400

    Adds copyright notice atop all files

--- src/lib/mcmc.py
+++ src/lib/mcmc.py
@@ -1,3 +1,26 @@
+# Copyright (C) 2014 Catherine Beauchemin <cbeau@users.sourceforge.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# =============================================================================
+#
+#                                   Preamble
+#
+# =============================================================================
+#
+
 from __future__ import print_function 
 from phymbie import emcee
 import h5py
@@ -7,6 +30,14 @@ import random
 import sys
 import time
 NegInf = float('-inf')
+
+#
+# =============================================================================
+#
+#                                   Utilities
+#
+# =============================================================================
+#
 
 
 def lnprobfn(pvec,model,*args,**kwargs):
--- src/lib/plot.py
+++ src/lib/plot.py
@@ -1,7 +1,29 @@
+# Copyright (C) 2014 Catherine Beauchemin <cbeau@users.sourceforge.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# =============================================================================
+#
+#                                   Preamble
+#
+# =============================================================================
+#
+
 import math
 import numpy
 import phymbie.mcmc
-
 ### plotting STUFF
 import matplotlib
 matplotlib.use('Agg')
@@ -17,6 +39,15 @@ params = {
 	'text.usetex': True
 }
 matplotlib.rcParams.update(params)
+
+#
+# =============================================================================
+#
+#                                   Utilities
+#
+# =============================================================================
+#
+
 
 class grid_plot(object):
 	def __init__(self, (gridheight,gridwidth)):
