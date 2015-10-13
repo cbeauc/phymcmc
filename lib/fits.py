687f96f020d41d4d07cc60702dea5f05bed84706
Catherine Beauchemin <cbeau@users.sourceforge.net>
2015-10-13 14:19:41 -0400

    Added linregress function to fits module

--- lib/fits.py
+++ lib/fits.py
@@ -120,3 +120,75 @@ def mock_yield_coeff(data):
 	vd = numpy.log10(data[:,1]).mean()-numpy.log10(data[:,1])
 	return (vd, math.log10(math.e)*td)
 
+
+def linregress(x, y=None):
+	"""
+	Calculate a regression line
+
+	This computes a least-squares regression for two sets of measurements.
+
+	Parameters
+	----------
+	x, y : array_like
+		two sets of measurements.  Both arrays should have the same length.
+		If only x is given (and y=None), then it must be a two-dimensional
+		array where one dimension has length 2.  The two sets of measurements
+		are then found by splitting the array along the length-2 dimension.
+
+	Returns
+	-------
+	slope : float
+		slope of the regression line
+	intercept : float
+		intercept of the regression line
+	full : dictionary
+		Contains
+			r2: R-squared
+			dslope: 1-sigma error on slope
+			dintercept: 1-sigma error on y-intercept
+
+	"""
+	TINY = 1.0e-20
+	if y is None:  # x is a (2, N) or (N, 2) shaped array_like
+		x = numpy.asarray(x)
+		if x.shape[0] == 2:
+			x, y = x
+		elif x.shape[1] == 2:
+			x, y = x.T
+		else:
+			msg = ("If only `x` is given as input, it has to be of shape "
+					"(2, N) or (N, 2), provided shape was %s" % str(x.shape))
+			raise ValueError(msg)
+	else:
+		x = numpy.asarray(x)
+		y = numpy.asarray(y)
+	n = len(x)
+	xmean = numpy.mean(x, None)
+	ymean = numpy.mean(y, None)
+
+	# average sum of squares:
+	ssxm, ssxym, ssyxm, ssym = numpy.cov(x, y, bias=1).flat
+	r_num = ssxym
+	r_den = numpy.sqrt(ssxm * ssym)
+	if r_den == 0.0:
+		r = 0.0
+	else:
+		r = r_num / r_den
+		# test for numerical error propagation
+		if r > 1.0:
+			r = 1.0
+		elif r < -1.0:
+			r = -1.0
+
+	df = n - 2
+	t = r * numpy.sqrt(df / ((1.0 - r + TINY)*(1.0 + r + TINY)))
+	#prob = 2 * distributions.t.sf(numpy.abs(t), df)
+	slope = ssxym / ssxm
+	intercept = ymean - slope*xmean
+	full = {}
+	full['r2'] = r**2.0
+	full['dslope'] = numpy.sqrt((1 - r**2) * ssym / ssxm / df)
+	full['dintercept'] = full['dslope']*numpy.sqrt(numpy.sum(x**2.0)/n)
+
+	return slope, intercept, full
+
